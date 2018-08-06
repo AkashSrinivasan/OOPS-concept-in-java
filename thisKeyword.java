b21fbc00717928151ef0e45dba98e339d92d57a2
Akash <37179506+AkashSrinivasan@users.noreply.github.com>
2018-08-06 19:47:13 +0530

    Create thisKeyword.java

---
+++ thisKeyword.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaoops;
+
+/**
+ *
+ * @author JANUS
+ */
+public class thisKeyword {  
+    int rollno;  
+    String name;  
+    float fee;  
+thisKeyword(int rollno,String name,float fee){  
+    this.rollno=rollno;  
+    this.name=name;  
+    this.fee=fee;  
+}  
+void display(){
+    System.out.println(rollno+" "+name+" "+fee);}  
+}  
+  
+class TestThis2{  
+    public static void main(String args[]){  
+        thisKeyword s1=new thisKeyword(111,"ankit",5000f);  
+        thisKeyword s2=new thisKeyword(112,"sumit",6000f);  
+        s1.display();  
+        s2.display();  
+}
+}  
+
