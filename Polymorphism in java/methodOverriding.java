a348e306828b2a6c17e8a01163b20508e31f30c4
Akash <37179506+AkashSrinivasan@users.noreply.github.com>
2018-08-07 12:41:13 +0530

    Create methodOverriding.java

---
+++ Polymorphism in java/methodOverriding.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaoops;
+
+/**
+ *
+ * @author JANUS
+ */
+public class methodOverriding {
+    void animals(){
+        System.out.println("the animals are dangerous");
+    }
+}
+class dogs extends methodOverriding{
+    void animals(){
+        System.out.println("dogs are cute");
+    }
+    public static void main(String[] args) {
+        dogs m = new dogs();
+        m.animals();
+    }
+}
+ 
